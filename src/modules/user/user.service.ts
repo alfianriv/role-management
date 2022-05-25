8348af8ff239c1236bef394bba2b45550be88c45
alfianriv <alfian.rivaldi8@gmail.com>
2022-05-25 23:33:13 +0700

    create api for users

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,26 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { RoleService } from '../role/role.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserRepository } from './repository/user.repository';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    private readonly repository: UserRepository,
+    @Inject(forwardRef(() => RoleService))
+    private readonly roleService: RoleService,
+  ) {}
+
+  create(data: CreateUserDto) {
+    return this.repository.save(data, { reload: true });
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.repository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.findOneById(id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, data: UpdateUserDto) {
+    await this.findOneById(id);
+    return this.repository.save({ id, ...data }, { reload: true });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    await this.findOneById(id);
+    const deleted = await this.repository.softDelete(id);
+    return { success: deleted.affected > 0 };
+  }
+
+  async findOneById(id: number) {
+    const user = await this.repository.findOne(id, {
+      relations: ['role'],
+    });
+    if (!user) throw new NotFoundException(`User with ID "${id}" not found`);
+    return user;
+  }
+
+  async assignRole(userId: number, roleId: number) {
+    const user = await this.findOneById(userId);
+    await this.roleService.findOneById(roleId);
+    user.roleId = roleId;
+    return this.repository.save(user, { reload: true });
   }
 }
