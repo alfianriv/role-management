6e244f7e7ea462b5946025162d732671b8a98205
alfianriv <alfian.rivaldi8@gmail.com>
2022-05-27 22:16:56 +0700

    add description api permission group

--- src/modules/permission-group/permission-group.controller.ts
+++ src/modules/permission-group/permission-group.controller.ts
@@ -21,26 +21,31 @@ export class PermissionGroupController {
     private readonly permissionGroupService: PermissionGroupService,
   ) {}
 
+  @ApiOperation({ summary: 'Create permission group' })
   @Post()
   create(@Body() data: CreatePermissionGroupDto) {
     return this.permissionGroupService.create(data);
   }
 
+  @ApiOperation({ summary: 'Find all permission group' })
   @Get()
   findAll() {
     return this.permissionGroupService.findAll();
   }
 
+  @ApiOperation({ summary: 'Find one permission group by id' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.permissionGroupService.findOne(Number(id));
   }
 
+  @ApiOperation({ summary: 'Update permission group by id' })
   @Put(':id')
   update(@Param('id') id: string, @Body() data: UpdatePermissionGroupDto) {
     return this.permissionGroupService.update(Number(id), data);
   }
 
+  @ApiOperation({ summary: 'Assign role to permission group' })
   @Put(':id/role/:roleId')
   assignRole(@Param('id') id: string, @Param('roleId') roleId: string) {
     return this.permissionGroupService.assignRole(Number(id), Number(roleId));
@@ -52,6 +57,7 @@ export class PermissionGroupController {
     return this.permissionGroupService.revokeRole(Number(id), Number(roleId));
   }
 
+  @ApiOperation({ summary: 'Delete permission group by id' })
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
   remove(@Param('id') id: string) {
